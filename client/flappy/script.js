ed62a1718496c1059a2161134f8f4b8b7d248ffb
maxwell-morris28 <mmorri2000@gmail.com>
2021-09-22 17:39:32 -0400

    Added jump and gravity

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -5,10 +5,31 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     let birdLeft = 220
     let birdBottom = 100
+    let gravity = 2
 
     function startGame() {
-        bird.style.bottom = birdBottom + 'px'
+        birdBottom -= gravity //gravity subtracts 2 from the birdBottom variable
+        bird.style.bottom = birdBottom + 'px' //constantly subtracts due to gravity; sets y position of bird
         bird.style.left = birdLeft + 'px'
     }
-    startGame()
+    let timerId = setInterval(startGame, 20) //interval allows startgame to run every 20 ms
+
+    //clearInterval(timerId) //stops timerId from running
+
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+        console.log(e.keyCode)
+        
+    }
+
+    function jump() {
+        if (birdBottom < 450) {
+        birdBottom += 50
+        bird.style.bottom = birdBottom + 'px' 
+        console.log(birdBottom)
+        }
+    }
+    document.addEventListener('keyup', control) //Every time your finger leaves the keyboard, jump is invoked
 })
