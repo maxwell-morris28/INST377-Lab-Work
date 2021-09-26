04814a3807e7fa1898e2660f2d73d74781a4ef3b
maxwell-morris28 <mmorri2000@gmail.com>
2021-09-26 00:40:52 -0400

    Added class-changing script for carousel functionality

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,2 +1,50 @@
-//test
-let test = 0;
+let slidePosition = 0;
+const slides = document.querySelectorAll('.carousel-item'); //creates array-like object of carousel items
+const totalSlides = slides.length;
+
+//console.log(slides.length);
+
+document.querySelector('#carousel-button--next')
+.addEventListener("click", () => {
+    moveToNextSlide();
+})
+
+document.querySelector('#carousel-button--prev')
+.addEventListener("click", () => {
+    moveToPrevSlide();
+})
+
+moveToNextSlide = () => {
+    //console.log('test');
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+        slides[slidePosition].classList.add("carousel-item--visible");
+        slides[totalSlides-1].classList.remove("carousel-item--visible")
+    }
+
+    else {
+        slidePosition++;
+        //console.log(slidePosition);
+        slides[slidePosition].classList.add("carousel-item--visible");
+        slides[slidePosition-1].classList.remove("carousel-item--visible")
+    }
+}
+
+moveToPrevSlide = () => {
+    //console.log('test');
+    if (slidePosition === 0) {
+        slidePosition = totalSlides-1;
+        slides[slidePosition].classList.add("carousel-item--visible");
+        slides[0].classList.remove("carousel-item--visible")
+    }
+
+    else {
+        slidePosition--;
+        console.log(slidePosition);
+        slides[slidePosition].classList
+        .add("carousel-item--visible");
+        slides[slidePosition+1].classList
+        .remove("carousel-item--visible")
+    }
+}
+    
