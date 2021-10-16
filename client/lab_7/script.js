44af656e9112f6648617e1209e749d80845de05e
maxwell-morris28 <mmorri2000@gmail.com>
2021-10-16 15:30:22 -0400

    test

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,7 +1,8 @@
 
 async function windowActions() {
     const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-    
+    const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
     const searchInput = document.querySelector('.search');
     const suggestions = document.querySelector('.suggestions');
     const request = await fetch(endpoint);
@@ -12,14 +13,15 @@ async function windowActions() {
     return restaurant.filter(type => {
         //figure out if the restaurant matches what was searched
         const regex = new RegExp(wordToMatch, 'gi');
-        return type.category.match(regex) ||
-        type.name.match(regex);
+        return type.zip.match(regex)
     });
     }
    function displayMatches() {
     const matchArray = findMatches(event.target.value, restaurant);
-    console.log(matchArray);
-    const html = matchArray.map(type => {
+    //console.log(matchArray);
+    const topFive = matchArray.slice(0, 5);
+    console.log(topFive);
+    const html = topFive.map(type => {
         return `
             <li>
               <span class="name">${type.name}</span><br>
@@ -30,11 +32,23 @@ async function windowActions() {
             </li>
         `;
     }).join('');
+    
     suggestions.innerHTML = html;
    }
    const restaurant = await request.json();
    searchInput.addEventListener('change', displayMatches);
    searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+   function mapInit() {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoibW1vcnJpMjAwMCIsImEiOiJja3V0NHpyNXY1bHh5MndtYWRybmM1bGwwIn0.wy9-oKCEeZoaPkYh3yGOiA'
+    }).addTo(mymap);
+   }
+   mapInit();
 }
 
 window.onload = windowActions;
